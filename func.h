4111e28e231ecdb9966025f0daa96720a15418d4
Jeongduckho <duckhoim@gmail.com>
2014-07-13 01:55:38 +0900

    단계별 프로젝트 3. 사용자로 부터 수 입력받기 * 작업내용 - 단계별 프로젝트 2 보완 - 단계별 프로젝트 1 보완 - 단계별 프로젝트 3

--- func.h
+++ func.h
@@ -1,6 +1,6 @@
 void mkrandnum(int i) {
-	int *n = NULL;
-	int p, q, check=0;
+	int * n = NULL;
+	int p, q, check = 0;
 	n = (int * ) malloc(sizeof(int) * i);
 	srand(time(NULL)); // To make different num at anytime
 	n[0] = rand() % 10; // To compare num, to except is better 
@@ -14,4 +14,28 @@ void mkrandnum(int i) {
 		}
 	}
 	free(n);
+}
+void getusernum(int i) {
+	int * user = NULL;
+	int p, q;
+	user = (int * ) malloc(sizeof(int) * i);
+	puts("자리수 만큼 한 자리씩 입력해주세요.");
+	puts("");
+	for (p = 0; p <= i; p++) {
+		printf("%d의 자리 : ", i + 1 - p);
+		scanf("%d", & user[p]);
+		if (user[p] >= 10) {
+			puts("10미만의 수만 입력가능합니다.");
+			p--;
+		}
+		for (q = -1; q < p; q++) {
+			if (user[p] == user[q]) {
+				puts("숫자가 중복되었습니다.");
+				puts(" 다른 수를 넣어주세요.");
+				p--;
+				break;
+			}
+		}
+	}
+	free(user);
 }
--- main.c
+++ main.c
@@ -4,29 +4,26 @@
 #include <time.h>
 #include "func.h"
 void main() {
-	int p /*counting*/
-	, i /*for level*/
-	;
+	int i /*for level*/;
 	puts("난이도를 선택하세요.");
-	puts("-----------------------------");
+	puts("-----------------------");
 	puts("[1].    두자리수");
 	puts("[2].    세자리수");
 	puts("[3].    네자리수");
-	puts("[4]. 다섯자리수");
-	puts("[5]. 여섯자리수");
-	puts("[6]. 일곱자리수");
-	puts("[7]. 여덟자리수");
-	puts("[8]. 아홉자리수");
+	puts("[4].  다섯자리수");
+	puts("[5].  여섯자리수");
+	puts("[6].  일곱자리수");
+	puts("[7].  여덟자리수");
+	puts("[8].  아홉자리수");
 	puts("[0].        끝내기");
-	puts("-----------------------------");
+	puts("----------------------");
 	printf(":");
 	scanf("%d", & i);
-	if (i == 0) {
-		system("exit");
-	}
-        mkrandnum(i);
-        main();
-        return; }
+	if (i == 0) { return; }
+	mkrandnum(i);
+        getusernum(i);
+	return;
+}
 
 
 
